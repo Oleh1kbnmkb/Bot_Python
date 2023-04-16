d00b98be7d26abc0ea4a67bc3e9a389c3bd77571
Oleh <op663246@gmail.com>
2023-04-16 10:19:51 +0300

    first Bot

---
+++ site_url.py
@@ -0,0 +1,102 @@
+computer = {
+    "Крісло АКЛАС Гілмор": {
+    "photo": "https://aklas.ua/storage/products/original/L8hyCYq4FRZP59Y1znsrKumRkSDFQb8Ayg2ENw9f.jpeg",
+    "price": "2.820грн"
+    },
+    "Крісло АКЛАС Сіті": {
+    "photo": "https://aklas.ua/storage/products/original/FZDN6AlqQELhjNVi0tL1hOWbdkqMC5kGKEToVrRt.jpeg",
+    "price": "4.680грн"
+    },
+    "Крісло АКЛАС Арсі": {
+    "photo": "https://aklas.ua/storage/products/original/blA37zlPCXMBmyKr2ZkFYILysVJMZvb49WbW10KS.jpeg",
+    "price": "3.150грн"
+    },
+    'Крісло конференційне АКЛАС Ларк': {
+    "photo": "https://aklas.ua/storage/products/original/0NjHYTY3coSMofReEqk9N8foPQk4uhcKNyigSx2k.jpeg",
+    "price": "2.510грн"
+    },
+    "Крісло АКЛАС Тета": {
+    "photo": "https://aklas.ua/storage/products/original/BulVt8KUVzfM720rblWT3prSdm4E6E2w5lUVXQvX.jpeg",
+    "price": "2.680грн"
+    },
+    "Диван Embawood Луна": {
+    "photo": "https://aklas.ua/storage/products/original/oioRCX915tJ6KS0ihmRZiPED7wPPusvFyCTKkiZ2.png",
+    "price": "11.960грн"
+    },
+    "Диван DLS Колібрі-3-КС": {
+    "photo": "https://aklas.ua/storage/products/original/FkNUKQfrlCO7490mv3kF7MZGSNiVEvjhldPQ1A7h.png",
+    "price": "12.609грн"
+    },
+    "Диван модульний RICHMAN ": {
+     "photo": "https://aklas.ua/storage/products/original/OXOB4HwVDI97hiRxFslw7lj8sweAY4fdr1Z99Bsh.jpeg",
+    "price": "11.886грн"
+    },
+    "Диван модульний": {
+    "photo": "https://aklas.ua/storage/products/original/MBZPkueYf32gbqBCBgrHZ7q1wdJH5PyGXGwZazHl.jpeg",
+    "price": "8.765грн"
+    },
+    "Диван DANIRO Лірик": {
+    "photo": "https://aklas.ua/storage/products/original/Mu4HfTokKu9xGKBk3pzlviPNZbcRMvDsGHi0GQoG.jpg",
+    "price": "16.940грн"
+    },
+    "Стіл M-Concept Серія Джет": {
+    "photo": "https://aklas.ua/storage/products/original/OY6l748fjVYmiPwZ1lgrUefEbKSNrRivEgXs32qB.jpg",
+    "price": "5.111грн"
+    },
+    "Стіл M-Concept Серія Атрибут":{
+    "photo": "https://aklas.ua/storage/products/original/JnOxwPCdBgHpbnmXEDTMfdSguwrR9N3gwBTUO0gv.jpg",
+    "price": "5.273грн"
+    },
+    "Стіл M-Concept Серія Сенс": {
+    "photo": "https://aklas.ua/storage/products/original/8K18RkV18soP5auftSMuphBnXV7jAwd2snof4BX9.jpg",
+    "price": "3.583грн"
+    },
+    "Стіл комп'ютерний СК-3 Kredens furniture":{
+    "photo": "https://www.dybok.com.ua/image/74167/stil-komputernij-sk-3-kredens-furniture-81048-product.jpg?v=0.1",
+    "price": "2.659грн"
+    },
+    "Стіл Бянко (графіт)": {
+    "photo": "https://www.dybok.com.ua/image/81803/stil-banko-grafit-svit-mebliv-product.jpg?v=0.1",
+    "price": "2.849грн"
+    },
+    "Столик журнальний Wudus Plump": {
+    "photo": "https://aklas.ua/storage/products/original/xIT6s504REzRzNDmWeGNc45Mikv9PzZcCuhUbEyR.jpg",
+    "price": "6.240грн"
+    },
+    "Столик журнальний ЧФЛИ Марс":{
+    "photo": "https://aklas.ua/storage/products/original/FQC58izuvEQk1tqlRInf2bKmOypYSU6s0QjgYlVl.png",
+    "price": "3.640грн"
+    },
+    "Стіл журнальний M-Concept": {
+    "photo": "https://aklas.ua/storage/products/original/elGmcIZi9oO1oWujNKZjIKUJJVgHNDZCaEmfLeeO.png",
+    "price": "4.390грн"
+    },
+    "Стіл журнальний Антонік":{
+    "photo": "https://aklas.ua/storage/products/original/QqoklNAKyJhLKlIVLECJtkSiGCWfwwQHtKBEb5TJ.jpeg",
+    "price": "6.490грн"
+    },
+    "Стіл журнальний Антонік ДС-2": {
+    "photo": "https://aklas.ua/storage/products/original/cjVf9aW0m0wR66TR240WbSBuJ6wPGGqZFj2eoFm8.jpeg",
+    "price": "4.150грн"
+    },
+    "Полка П-27-53": {
+    "photo": "https://www.meblium.com.ua/images/product/_/-/min/450_450-_-_27__.jpg",
+    "price": "3.121грн"
+    },
+    "Книжкова полиця ПК-2":{
+    "photo": "https://cdn.27.ua/sc--media--prod/default/94/1c/d1/941cd10b-f899-4858-8e6c-63f4ec386de0.jpg",
+    "price": "1.103грн"
+    },
+    "Полка декоративная DC ДС-4": {
+    "photo": "https://cdn.27.ua/799/3d/a0/408992_3.jpeg",
+    "price": "847грн"
+    },
+    "Полиця навісна на стіну П-1":{
+    "photo": "https://images.prom.ua/3399179361_w640_h640_polka-navesnaya-na.jpg",
+    "price": "911грн"
+    },
+    "Полка Forte Erika для книг": {
+    "photo": "https://cdn.27.ua/sc--media--prod/default/5d/11/c1/5d11c1b6-83b0-4e68-8dac-0d2b3f742129.jpg",
+    "price": "390грн"
+    }
+}
---
+++ site_url2.py
@@ -0,0 +1,22 @@
+table = {
+    "Стіл M-Concept Серія Джет": {
+    "photo": "https://aklas.ua/storage/products/original/OY6l748fjVYmiPwZ1lgrUefEbKSNrRivEgXs32qB.jpg",
+    "price": "5.111грн"
+    },
+    "Стіл M-Concept Серія Атрибут":{
+    "photo": "https://aklas.ua/storage/products/original/JnOxwPCdBgHpbnmXEDTMfdSguwrR9N3gwBTUO0gv.jpg",
+    "price": "5.273грн"
+    },
+    "Стіл M-Concept Серія Сенс": {
+    "photo": "https://aklas.ua/storage/products/original/8K18RkV18soP5auftSMuphBnXV7jAwd2snof4BX9.jpg",
+    "price": "3.583грн"
+    },
+    "Стіл комп'ютерний СК-3 Kredens furniture":{
+    "photo": "https://www.dybok.com.ua/image/74167/stil-komputernij-sk-3-kredens-furniture-81048-product.jpg?v=0.1",
+    "price": "2.659грн"
+    },
+    "Стіл письмовий-BIU_130 Коен Нові направляючі Гербор": {
+    "photo": "https://www.dybok.com.ua/image/76070/stil-pismovij-biu130-koen-novi-napravlauci-gerbor-product.jpg?v=0.1",
+    "price": "7.430грн"
+    }
+}
