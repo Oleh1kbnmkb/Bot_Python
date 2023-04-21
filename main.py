350dccb0ef7501d9cc0d60e02d00a58f31654140
Oleh <op663246@gmail.com>
2023-04-21 15:40:20 +0300

    Bot

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from site_url import computer
+from site_url import furniture
+
 
 import os
 from dotenv import load_dotenv
@@ -23,6 +24,7 @@ dp = Dispatcher(bot=bot)
 
 
 
+
 @dp.message_handler(commands=['start'])
 async def command_start(message: types.Message):
     await bot.send_message(message.from_user.id, text ='Раді вітати вас у інтернет магазині Mabel Store.🎆🎇\nЦей бот допоможе вам вибрати товар за невеличким описом та з економить ваш час при виборі товару.😀', reply_markup=nav.BotMenu)
@@ -37,6 +39,7 @@ async def set_default_commands(dp):
      await bot.set_my_commands(
           [
           types.BotCommand('start', 'Запустити бота'),
+          types.BotCommand('add_goods', 'Добавити товар у категорію Офісні крісла')
           ]
      )
 
@@ -47,7 +50,11 @@ async def set_default_commands(dp):
 @dp.message_handler()
 async def bot_message(message: types.Message):
     if message.text == "Магазин":
-        await bot.send_message(message.from_user.id, text = "Оберіть категорію товару, який хочете придбати", reply_markup=nav.OtherMenu)
+        await bot.send_message(message.from_user.id, text = "Оберіть категорію товару, який хочете придбати", reply_markup=nav.OtherMenu1)
+    elif message.text == "Меблі":
+         await bot.send_message(message.from_user.id, text = "Меблі", reply_markup=nav.OtherMenu)
+    elif message.text == "Назад":
+         await bot.send_message(message.from_user.id, text = "Назад", reply_markup=nav.OtherMenu1)
     elif message.text == "Офісні крісла":
         await bot.send_message(message.from_user.id, text = "Офісні крісла", reply_markup=nav.goods)
     elif message.text == "Головне меню":
@@ -60,27 +67,81 @@ async def bot_message(message: types.Message):
         await bot.send_message(message.from_user.id, text = "Журнальні столи", reply_markup=nav.magazine_tables)
     elif message.text == "Полиці для книг":
           await bot.send_message(message.from_user.id, text = "Полиці для книг", reply_markup=nav.book_shelves)
+    elif message.text == "Шафи купе":
+          await bot.send_message(message.from_user.id, text = "Шафи купе", reply_markup=nav.cabinet)
+    elif message.text == "Комп'ютерна техніка":
+          await bot.send_message(message.from_user.id, text="Комп'ютерна техніка", reply_markup=nav.LaptopMenu)
+    elif message.text == "Ноутбуки":
+          await bot.send_message(message.from_user.id, text="Ноутбуки", reply_markup=nav.laptop1)
+    elif message.text == "Назад":
+          await bot.send_message(message.from_user.id, text="Назад", reply_markup=nav.OtherMenu1)
+    elif message.text == "Комп'ютери":
+          await bot.send_message(message.from_user.id, text="Комп'ютери", reply_markup=nav.computers)
+    elif message.text == "Телевізори":
+          await bot.send_message(message.from_user.id, text="Телевізори", reply_markup=nav.tv_menu)
+    elif message.text == "Офіс, школа книги":
+          await bot.send_message(message.from_user.id, text="Офіс, школа книги", reply_markup=nav.SchoolMenu)
+    elif message.text == "Рюкзаки":
+          await bot.send_message(message.from_user.id, text="Рюкзаки", reply_markup=nav.back2)
+    elif message.text == "Шкільне приладдя":
+          await bot.send_message(message.from_user.id, text="Шкільне приладдя", reply_markup=nav.school_ba)
+    elif message.text == "Паперова продукція":
+          await bot.send_message(message.from_user.id, text="Паперова продукція", reply_markup=nav.paper1)
+    elif message.text == "Зоотовари":
+          await bot.send_message(message.from_user.id, text="Зоотовари", reply_markup=nav.PetMenu)
+    elif message.text == "Собака":
+          await bot.send_message(message.from_user.id, text="Собака", reply_markup=nav.goods_dog)
+    elif message.text == "Кіт":
+          await bot.send_message(message.from_user.id, text="Кіт", reply_markup=nav.goods_cat)
+    elif message.text == "Побутова техніка":
+          await bot.send_message(message.from_user.id, text="Побутова техніка", reply_markup=nav.houseMenu)
+    elif message.text == "Холодильники":
+          await bot.send_message(message.from_user.id, text="Холодильники", reply_markup=nav.cold1)
+    elif message.text == "Роботи пилососи":
+          await bot.send_message(message.from_user.id, text="Роботи пилососи", reply_markup=nav.clear1)
+    elif message.text == "Пральні машини":
+          await bot.send_message(message.from_user.id, text="Пральні машини", reply_markup=nav.car6)
+    elif message.text == "Рюкзаки":
+          await bot.send_message(message.from_user.id, text="Рюкзаки", reply_markup=nav.back2)
+    elif message.text == "Шкільне приладдя":
+          await bot.send_message(message.from_user.id, text="Шкільне приладдя", reply_markup=nav.school_ba)
+    elif message.text == "Паперова продукція":
+          await bot.send_message(message.from_user.id, text="Паперова продукція", reply_markup=nav.paper1)
 
-          computer_shop = InlineKeyboardMarkup()
-          for teh in computer_shop:
-                button = InlineKeyboardButton(text = computer, callback_data=computer)
-                computer_shop.add(button)
+
+          furniture_shop = InlineKeyboardMarkup()
+          for fur in furniture_shop:
+              button = InlineKeyboardButton(text = furniture, callback_data=furniture)
+              furniture_shop.add(button)
+
+
+        
+
+
+
+
+
+         
 
 
           
 
          
 @dp.callback_query_handler()
-async def computer_info_handler(callback_query: types.CallbackQuery):
-     if callback_query.data in computer.keys():
-          computer_name = computer[callback_query.data]["name"]
-          url = computer[callback_query.data]["site_url"]
-          computer_description = computer[callback_query.data]["description"]
-          computer_price = computer[callback_query.data]["price"]
-          message = f"<b>Назва: </b> {computer_name}\n<b>Посилання на магазин: </b> {url}\n\n<b>Опис: </b> {computer_description}\n\n<b>Ціна:</b> {computer_price}"
+async def furniture_info_handler(callback_query: types.CallbackQuery):
+     if callback_query.data in furniture.keys():
+          await bot.send_photo(callback_query.message.chat.id, furniture[callback_query.data]["photo"])
+          furniture_name = furniture[callback_query.data]["name"]
+          url = furniture[callback_query.data]["site_url"]
+          furniture_description = furniture[callback_query.data]["description"]
+          furniture_price = furniture[callback_query.data]["price"]
+          message = f"<b>Назва: </b> {furniture_name}\n<b>Посилання на магазин: </b> {url}\n\n<b>Опис: </b> {furniture_description}\n\n<b>Ціна:</b> {furniture_price}"
           await bot.send_message(callback_query.message.chat.id, message, parse_mode='html')
      else:
           await bot.send_message(callback_query.message.chat.id, "Такого товару нажаль немає в наявності😟")
+
+
+
 
 
 
@@ -95,5 +156,5 @@ async def on_startup(dp):
   
   
 if __name__ == '__main__':
-     executor.start_polling(dp)
+     executor.start_polling(dp, on_startup=on_startup)
     
--- markups.py
+++ markups.py
@@ -11,9 +11,73 @@ btn1 = KeyboardButton("Магазин")
 BotMenu = ReplyKeyboardMarkup(resize_keyboard=True)
 BotMenu.add(btn1)
 
+back5 = KeyboardButton("Назад")
+
+# --- Bot Other Menu 1 ---
+shop_goods1 = ReplyKeyboardMarkup()
+fur = KeyboardButton("Меблі")
+teh = KeyboardButton("Комп'ютерна техніка")
+tv = KeyboardButton("Телевізори")
+backpacks = KeyboardButton("Офіс, школа книги")
+pet_products = KeyboardButton("Зоотовари")
+household = KeyboardButton("Побутова техніка")
+OtherMenu1 = ReplyKeyboardMarkup(resize_keyboard=True)
+OtherMenu1.add(teh, tv, fur)
+OtherMenu1.add(household, pet_products, backpacks)
+OtherMenu1.add(btnMenu)
 
 
-# back = KeyboardButton(text = "Назад")
+
+# --- backpacks ----
+backpacks1 = ReplyKeyboardMarkup()
+back1 = KeyboardButton("Рюкзаки")
+school_b = KeyboardButton("Шкільне приладдя")
+paper = KeyboardButton("Паперова продукція")
+SchoolMenu = ReplyKeyboardMarkup(resize_keyboard=True)
+SchoolMenu.add(back1, school_b)
+SchoolMenu.add(paper, back5)
+
+
+
+
+# --- pet_products ----
+pet_products1 = ReplyKeyboardMarkup()
+dog = KeyboardButton("Собака")
+cat = KeyboardButton("Кіт")
+PetMenu = ReplyKeyboardMarkup()
+PetMenu.add(dog, cat)
+PetMenu.add(back5)
+
+
+
+
+
+# --- household ---
+household1 = ReplyKeyboardMarkup()
+cold = KeyboardButton("Холодильники")
+clear = KeyboardButton("Роботи пилососи")
+car = KeyboardButton("Пральні машини")
+houseMenu = ReplyKeyboardMarkup()
+houseMenu.add(cold, clear)
+houseMenu.add(car, back5)
+
+
+
+
+
+
+laptop = ReplyKeyboardMarkup()
+lap = KeyboardButton("Ноутбуки")
+comp = KeyboardButton("Комп'ютери")
+LaptopMenu = ReplyKeyboardMarkup(resize_keyboard=True)
+LaptopMenu.add(lap, comp)
+LaptopMenu.add(back5)
+
+
+
+
+
+back = KeyboardButton(text = "Назад")
 
 # --- Bot Other Menu ---
 shop_goods = ReplyKeyboardMarkup()
@@ -22,13 +86,12 @@ technique1 = KeyboardButton("Дивани")
 home_appliances = KeyboardButton("Робочі столи")
 home_goods = KeyboardButton("Журнальні столи")
 instruments = KeyboardButton("Полиці для книг")
-
-
-
+cabinet1 = KeyboardButton("Шафи купе")
 OtherMenu = ReplyKeyboardMarkup(resize_keyboard=True)
 OtherMenu.add(technique, technique1)
 OtherMenu.add(home_appliances, home_goods) 
-OtherMenu.add(instruments, btnMenu)
+OtherMenu.add(instruments, cabinet1)
+OtherMenu.add(back)
 
 
 
@@ -92,3 +155,136 @@ book_shelves = InlineKeyboardMarkup(
     ]
 )
 
+
+cabinet = InlineKeyboardMarkup(
+    inline_keyboard=[
+    [InlineKeyboardButton(text = "Шафа купе ДІМ В-104", callback_data="Шафа купе ДІМ В-104")],
+    [InlineKeyboardButton(text = "Шафа купе ДІМ В-146", callback_data="Шафа купе ДІМ В-146")],
+    [InlineKeyboardButton(text = "Шафа купе ДІМ ВН-486", callback_data="Шафа купе ДІМ ВН-486")],
+    [InlineKeyboardButton(text = "Шафа купе ДІМ В-106", callback_data="Шафа купе ДІМ В-106")],
+    [InlineKeyboardButton(text = "Шафа купе ДІМ Камелот КН 25", callback_data="Шафа купе ДІМ Камелот КН 25")]
+    ]
+)
+
+
+laptop1 = InlineKeyboardMarkup(
+    inline_keyboard=[
+    [InlineKeyboardButton(text = "Ноутбук Acer Aspire 5 ", callback_data="Ноутбук Acer Aspire 5 ")],
+    [InlineKeyboardButton(text = "Ноутбук Lenovo IdeaPad L3", callback_data="Ноутбук Lenovo IdeaPad L3")],
+    [InlineKeyboardButton(text = "Ноутбук Apple MacBook Air 13", callback_data="Ноутбук Apple MacBook Air 13")],
+    [InlineKeyboardButton(text = "Ноутбук ASUS Vivobook 16X", callback_data="Ноутбук ASUS Vivobook 16X")],
+    [InlineKeyboardButton(text = "Ноутбук HP Laptop 15s", callback_data="Ноутбук HP Laptop 15s")]
+    ]
+)
+
+
+
+computers = InlineKeyboardMarkup(
+    inline_keyboard=[
+    [InlineKeyboardButton(text = "Комп'ютер Asus S500MC", callback_data="Комп'ютер Asus S500MC")],
+    [InlineKeyboardButton(text = "Комп'ютер Cobra Optimal", callback_data="Комп'ютер Cobra Optimal")],
+    [InlineKeyboardButton(text = "Ігровий Athlon X4 970", callback_data="Ігровий Athlon X4 970")],
+    [InlineKeyboardButton(text = "Комп'ютер Apple Mac Studio M1", callback_data="Комп'ютер Apple Mac Studio M1")],
+    [InlineKeyboardButton(text = "Комп'ютер ARTLINE Gaming X47", callback_data="Комп'ютер ARTLINE Gaming X47")]
+    ]
+)
+
+
+tv_menu = InlineKeyboardMarkup(
+    inline_keyboard=[
+    [InlineKeyboardButton(text = "Телевізор Samsung UE50", callback_data="Телевізор Samsung UE50")],
+    [InlineKeyboardButton(text = "Телевизор LG 55", callback_data="Телевизор LG 55")],
+    [InlineKeyboardButton(text = "Телевизор LG 32L", callback_data="Телевизор LG 32L")],
+    [InlineKeyboardButton(text = "Телевизор SETUP 24", callback_data="Телевизор SETUP 24")],
+    [InlineKeyboardButton(text = "Телевизор Ergo 43G", callback_data="Телевизор Ergo 43G")],
+    [InlineKeyboardButton(text = "Телевизор Nokia Smart TV", callback_data="Телевизор Nokia Smart TV")]
+    ]
+)
+
+goods_dog = InlineKeyboardMarkup(
+    inline_keyboard=[
+    [InlineKeyboardButton(text = "Корми для собак", callback_data="Корми для собак")],
+    [InlineKeyboardButton(text = "Посуд для собак", callback_data="Посуд для собак")],
+    [InlineKeyboardButton(text = "Одяг для собак", callback_data="Одяг для собак")],
+    [InlineKeyboardButton(text = "Іграшки для собак", callback_data="Іграшки для собак")],
+    [InlineKeyboardButton(text = "Спальні місця для собак", callback_data="Спальні місця для собак")]
+    ]
+)
+
+
+goods_cat = InlineKeyboardMarkup(
+    inline_keyboard=[
+    [InlineKeyboardButton(text = "Корми для котів", callback_data="Корми для котів")],
+    [InlineKeyboardButton(text = "Посуд для котів", callback_data="Посуд для котів")],
+    [InlineKeyboardButton(text = "Наповнювачі для котів", callback_data="Наповнювачі для котів")],
+    [InlineKeyboardButton(text = "Іграшки для котів", callback_data="Іграшки для котів")],
+    [InlineKeyboardButton(text = "Спальні місця для котів", callback_data="Спальні місця для котів")]
+    ]
+)
+
+
+
+cold1 = InlineKeyboardMarkup(
+    inline_keyboard=[
+    [InlineKeyboardButton(text = "Холодильник SAMSUNG RB38", callback_data="Холодильник SAMSUNG RB38")],
+    [InlineKeyboardButton(text = "Холодильник BOSCH KGN36", callback_data="Холодильник BOSCH KGN36")],
+    [InlineKeyboardButton(text = "Холодильник Whirlpool W7X", callback_data="Холодильник Whirlpool W7X")],
+    [InlineKeyboardButton(text = "Холодильник BEKO RCNA40", callback_data="Холодильник BEKO RCNA40")],
+    [InlineKeyboardButton(text = "Холодильник GORENJE RK62", callback_data="Холодильник GORENJE RK62")]
+    ]
+)
+
+
+
+clear1 = InlineKeyboardMarkup(
+    inline_keyboard=[
+    [InlineKeyboardButton(text = "Робот-пылесос Rowenta X-PLORER", callback_data="Робот-пылесос Rowenta X-PLORER")],
+    [InlineKeyboardButton(text = "Робот-пилосос Rowenta X-PLORER S130AI", callback_data="Робот-пилосос Rowenta X-PLORER S130AI")],
+    [InlineKeyboardButton(text = "Робот-пилосос Roborock Vacuum Cleaner", callback_data="Робот-пилосос Roborock Vacuum Cleaner")],
+    [InlineKeyboardButton(text = "Робот-пилосос ECOVACS DEEBOT OZMO", callback_data="Робот-пилосос ECOVACS DEEBOT OZMO")],
+    [InlineKeyboardButton(text = "Робот-пилосос ECOVACS Deebot OZMO T8 AIVI", callback_data="Робот-пилосос ECOVACS Deebot OZMO T8 AIVI")]
+    ]
+)
+
+
+car6 = InlineKeyboardMarkup(
+    inline_keyboard=[
+    [InlineKeyboardButton(text = "Пральна машина вузька WHIRLPOOL", callback_data="Пральна машина вузька WHIRLPOOL")],
+    [InlineKeyboardButton(text = "Пральна машина вузька SAMSUNG ", callback_data="Пральна машина вузька SAMSUNG ")],
+    [InlineKeyboardButton(text = "Пральна машина вузька INDESIT ", callback_data="Пральна машина вузька INDESIT")]
+    ]
+)
+
+
+back2 = InlineKeyboardMarkup(
+    inline_keyboard=[
+    [InlineKeyboardButton(text = "Рюкзаки для дівчат", callback_data="Рюкзаки для дівчат")],
+    [InlineKeyboardButton(text = "Рюкзаки для хлопців", callback_data="Рюкзаки для хлопців")],
+    
+    ]
+)
+
+
+
+school_ba = InlineKeyboardMarkup(
+    inline_keyboard=[
+    [InlineKeyboardButton(text = "Пенали", callback_data="Пенали")],
+    [InlineKeyboardButton(text = "Зошити", callback_data="Зошити")],
+    [InlineKeyboardButton(text = "Фломастери", callback_data="Фломастери")],
+    [InlineKeyboardButton(text = "Щоденники", callback_data="Щоденники")],
+    [InlineKeyboardButton(text = "Пластилін", callback_data="Пластилін")],
+    [InlineKeyboardButton(text = "Альбоми для малювання", callback_data="Альбоми для малювання")]
+    ]
+)
+
+
+paper1 = InlineKeyboardMarkup(
+    inline_keyboard=[
+    [InlineKeyboardButton(text = "Папір офісний", callback_data="Папір офісний")],
+    [InlineKeyboardButton(text = "Блокноти, зошити офісні", callback_data="Блокноти, зошити офісні")],
+    [InlineKeyboardButton(text = "Календарі", callback_data="Календарі")],
+    [InlineKeyboardButton(text = "Папір для нотаток", callback_data="Папір для нотаток")],
+    [InlineKeyboardButton(text = "Кольоровий папір", callback_data="Кольоровий папір")],
+    [InlineKeyboardButton(text = "Папір спеціальний", callback_data="Папір спеціальний")]
+    ]
+)
